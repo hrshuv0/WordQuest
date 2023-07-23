e1f9439617af4fdc01f3d7a254c93aa52e682c8a
hrshuv0 <h.r.shuvo9@gmail.com>
2023-07-24 00:30:38 +0600

    add view

---
+++ API/Areas/Admin/Controllers/WordQuestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.Admin.Controllers;
+
+[Area("admin")]
+public class WordQuestController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
