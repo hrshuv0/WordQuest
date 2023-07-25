ec13829e9c5fd90956fb446b5c3a6bc3901fe9bb
hrshuv0 <h.r.shuvo9@gmail.com>
2023-07-25 20:51:16 +0600

    update data table design

--- API/wwwroot/js/vocabulary.js
+++ API/wwwroot/js/vocabulary.js
@@ -8,12 +8,18 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "processing": true,
         "serverSide": true,
+        "retrieve": true,
+        "responsive": true,
         "ajax": {
             "url": "/Admin/Vocabulary/GetAll",            
         },
         "columns": [
+            { "data": "id", "width": "30%" },
             { "data": "name", "width": "30%" },
             { "data": "status", "width": "30%" },
         ],
+        "pagingType": 'numbers',
+        "ordering": false
+        
     });
 }
