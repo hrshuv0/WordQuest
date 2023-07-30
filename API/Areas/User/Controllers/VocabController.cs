c9505caf2505de05cfb582689c499f30b273084f
hrshuv0 <h.r.shuvo9@gmail.com>
2023-07-30 15:23:44 +0600

    some fix to user side theme

---
+++ API/Areas/User/Controllers/VocabController.cs
@@ -0,0 +1,14 @@
+﻿using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Areas.User.Controllers;
+
+[Area("User")]
+public class VocabController : BaseMvcController
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
