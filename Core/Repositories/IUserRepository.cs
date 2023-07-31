4a24d76c5349afc2c8bccc1caf7de781559bcccd
hrshuv0 <h.r.shuvo9@gmail.com>
2023-07-31 17:03:45 +0600

    add user service, repository

---
+++ Core/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Identity;
+
+namespace Core.Repositories;
+
+public interface IUserRepository
+{
+    Task Add<T>(T entity);
+    Task Delete<T>(T entity);
+    Task<bool> SaveAll();
+    Task<ApplicationUser> Get(string id);
+    Task<List<ApplicationUser>> Load(int pageNumber, int pageSize);
+
+} 
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using Core.Entities.Identity;
+using Core.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    #region CONFIG
+    private readonly ApplicationDbContext? _context;
+
+    public UserRepository(ApplicationDbContext? context)
+    {
+        _context = context;
+    }
+
+    #endregion
+    
+    public async Task Add<T>(T entity)
+    {
+        await _context!.AddAsync(entity!);
+    }
+
+    public async Task Delete<T>(T entity)
+    {
+        _context!.Remove(entity!);
+    }
+
+    public async Task<bool> SaveAll()
+    {
+        return await _context!.SaveChangesAsync() > 0;
+    }
+
+    public async Task<ApplicationUser> Get(string id)
+    {
+        var user = await _context!.ApplicationUser!
+            .FirstOrDefaultAsync(u => u.Id == id);
+
+        return user!;
+    }
+
+    public async Task<List<ApplicationUser>> Load(int pageNumber, int pageSize)
+    {
+        IQueryable<ApplicationUser> query = _context!.ApplicationUser!.AsQueryable();
+        
+        query = query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
+        
+        var result = await query.ToListAsync();
+        
+        return result;
+    }
+    
+}
